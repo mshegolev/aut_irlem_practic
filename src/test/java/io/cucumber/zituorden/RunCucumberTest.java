d8a61f39427c8437bfdd20808f355d7834360db9
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2020-06-29 01:05:06 +0300

    fix test

--- src/test/java/io/cucumber/zituorden/RunCucumberTest.java
+++ src/test/java/io/cucumber/zituorden/RunCucumberTest.java
@@ -5,8 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"},
-        glue = {"com/adaptavist/tm4j/cucumberExample/stepDefinition"})
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"})
 
 public class RunCucumberTest {
 
