754a78632b82ee33cb7568eb8126c76ea8a18658
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2020-06-29 00:32:24 +0300

    add auth test

--- src/test/java/io/cucumber/zituorden/RunCucumberTest.java
+++ src/test/java/io/cucumber/zituorden/RunCucumberTest.java
@@ -5,10 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"},
-        features="src/test/resources/io/cucumber/zituorden",
-        tags = "@all",
-        dryRun = false)
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"})
+
 public class RunCucumberTest {
 
 }
